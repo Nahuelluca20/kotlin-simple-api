60a7b481dd8c903debb9d7baefdd86f804415343
Nahuel Luca <nahuelluca20@gmail.com>
2024-05-03 18:38:20 -0300

    Add get customer by id

--- src/main/kotlin/com/example/models/Customer.kt
+++ src/main/kotlin/com/example/models/Customer.kt
@@ -5,4 +5,8 @@ import kotlinx.serialization.Serializable
 @Serializable // this means, class Customer can be serializable and de deserialized
 data class Customer (val id:String, val firsName: String, val lastName: String, val email: String)
 
+// val: The val keyword in Kotlin is used to declare a read-only variable,
+// which means that once it is initialized, the variable's reference to another
+// object cannot be changed. However, the contents of the object referenced by the variable
+// may change if the object is mutable.
 val customerStorage = mutableListOf<Customer>()
