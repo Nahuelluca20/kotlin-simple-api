21cead2beea173ebec84e7bd31c68cd476e8050b
Nahuel Luca <nahuelluca20@gmail.com>
2024-05-04 13:10:36 -0300

    Add order and OrderItem model

--- src/main/kotlin/com/example/models/Customer.kt
+++ src/main/kotlin/com/example/models/Customer.kt
@@ -3,7 +3,7 @@ package com.example.models
 import kotlinx.serialization.Serializable
 
 @Serializable // this means, class Customer can be serializable and de deserialized
-data class Customer (val id:String, val firsName: String, val lastName: String, val email: String)
+data class Customer(val id: String, val firsName: String, val lastName: String, val email: String)
 
 // val: The val keyword in Kotlin is used to declare a read-only variable,
 // which means that once it is initialized, the variable's reference to another
