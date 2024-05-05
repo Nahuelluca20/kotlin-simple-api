ec5f37fff9761f22b61ac1994374e4f26a96d310
Nahuel Luca <nahuelluca20@gmail.com>
2024-05-05 15:07:24 -0300

    remove unused imports

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -5,7 +5,6 @@ import com.example.routes.getOrderRoute
 import com.example.routes.listOrdersRoute
 import com.example.routes.totalizeOrderRoute
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
--- src/main/kotlin/com/example/routes/OrderRoutes.kt
+++ src/main/kotlin/com/example/routes/OrderRoutes.kt
@@ -4,7 +4,6 @@ import com.example.models.Order
 import com.example.models.orderStorage
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
