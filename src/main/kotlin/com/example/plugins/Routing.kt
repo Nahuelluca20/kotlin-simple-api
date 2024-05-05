cb436959b2dc31914a1ee149c7f587d1b93f0dfd
Nahuel Luca <nahuelluca20@gmail.com>
2024-05-05 15:37:31 -0300

    add post route

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,9 +1,6 @@
 package com.example.plugins
 
-import com.example.routes.customerRouting
-import com.example.routes.getOrderRoute
-import com.example.routes.listOrdersRoute
-import com.example.routes.totalizeOrderRoute
+import com.example.routes.*
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
@@ -13,5 +10,6 @@ fun Application.configureRouting() {
         listOrdersRoute()
         getOrderRoute()
         totalizeOrderRoute()
+        postOrder()
     }
 }
--- src/main/kotlin/com/example/routes/OrderRoutes.kt
+++ src/main/kotlin/com/example/routes/OrderRoutes.kt
@@ -4,6 +4,7 @@ import com.example.models.Order
 import com.example.models.orderStorage
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -40,10 +41,43 @@ fun Route.totalizeOrderRoute() {
     }
 }
 
+fun Route.postOrder() {
+    post {
+        val order = call.receive<Order>()
+        orderStorage.add(order)
+
+        call.respondText("Order added correctly", status = HttpStatusCode.Created)
+    }
+}
+
 fun getOrderById(call: ApplicationCall): Order? {
     val id = call.parameters["id"] ?: return null
     val order = orderStorage.find { it.number == id } ?: return null
 
     return order
 }
+
+
+//    route("/order") {
+//        get {
+//            if (orderStorage.isNotEmpty()) {
+//                call.respond(orderStorage)
+//            } else {
+//                call.respondText("No orders found", status = HttpStatusCode.OK)
+//            }
+//        }
+//
+//        get("{id?}") {}
+//
+//        get("{id?}/total") {}
+//
+//        post {
+//            val order = call.receive<Order>()
+//            orderStorage.add(order)
+//
+//            call.respondText("Order added correctly", status = HttpStatusCode.Created)
+//        }
+//
+//        delete { }
+//    }
 
