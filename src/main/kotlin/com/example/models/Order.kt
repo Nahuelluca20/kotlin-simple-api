669f85dad76c1a0697446c0c22f3b876719aec54
Nahuel Luca <nahuelluca20@gmail.com>
2024-05-05 15:06:38 -0300

    Add orders routes and 2 spaces ident

--- src/main/kotlin/com/example/models/Order.kt
+++ src/main/kotlin/com/example/models/Order.kt
@@ -3,7 +3,9 @@ package com.example.models
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Order(val number: String, val content: List<OrderItem> )
+data class Order(val number: String, val contents: List<OrderItem>)
 
 @Serializable
 data class OrderItem(val item: String, val amount: Int, val price: Double)
+
+val orderStorage = mutableListOf<Order>()
---
+++ src/main/kotlin/com/example/routes/OrderRoutes.kt
@@ -0,0 +1,50 @@
+package com.example.routes
+
+import com.example.models.Order
+import com.example.models.orderStorage
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.listOrdersRoute() {
+  get("order") {
+    if (orderStorage.isNotEmpty()) {
+      call.respond(orderStorage)
+    } else {
+      call.respondText("No orders found", status = HttpStatusCode.OK)
+    }
+  }
+}
+
+fun Route.getOrderRoute() {
+  get("/order/{id?}") {
+    val order = getOrderById(call) ?: return@get call.respondText(
+      "Order not found",
+      status = HttpStatusCode.NotFound
+    )
+
+    call.respond(order)
+  }
+}
+
+fun Route.totalizeOrderRoute() {
+  get("order/{id?}/total") {
+    val order = getOrderById(call) ?: return@get call.respondText(
+      "Order not found",
+      status = HttpStatusCode.NotFound
+    )
+    val total = order.contents.sumOf { it.price * it.amount }
+
+    call.respond(total)
+  }
+}
+
+fun getOrderById(call: ApplicationCall): Order? {
+  val id = call.parameters["id"] ?: return null
+  val order = orderStorage.find { it.number == id } ?: return null
+
+  return order
+}
+
